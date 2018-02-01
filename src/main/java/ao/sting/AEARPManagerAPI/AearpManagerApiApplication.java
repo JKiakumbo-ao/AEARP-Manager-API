1c5410a54e8e8b40e80866cb9c28d2299eb029ee
JKiakumbo.ao <jkiakumbo.ao@onet.pl>
2018-02-01 12:50:03 +0100

    create project

---
+++ src/main/java/ao/sting/AEARPManagerAPI/AearpManagerApiApplication.java
@@ -0,0 +1,12 @@
+package ao.sting.AEARPManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AearpManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AearpManagerApiApplication.class, args);
+	}
+}
